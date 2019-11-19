e4dededb38719cb60210c456e5ba5bbfaec1a855
Fatih Beyhan <fbeyhan@gmail.com>
2019-11-18 23:59:27 -0500

    this is commit from 2nd person

---
+++ src/test/java/com/nationwide/runners/RegressionRunner1.java
@@ -0,0 +1,13 @@
+package com.nationwide.runners;
+
+import org.junit.Test;
+
+public class RegressionRunner1 {
+
+    @Test
+    public void test1(){
+        System.out.println("test1");
+    }
+
+
+}
